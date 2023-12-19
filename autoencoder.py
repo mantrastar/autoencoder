ba36de0fe02354da705d50adff8d2c5cce92c11e
Ven <ven@mantra.io>
2023-12-19 08:31:56 -0800

    Update

--- autoencoder.py
+++ autoencoder.py
@@ -12,7 +12,11 @@ import random as rnd
 import numpy as np
 import pandas as pd
 import sklearn as sk
+import sklearn.svm as svm
 import torch as tor
+import torch.nn as nn
+import torch.optim as opt
+from torch.utils.data import DataLoader, TensorDataset
 import skorch as skr
 import seaborn as sea
 import matplotlib as mpl
@@ -24,11 +28,15 @@ import matplotlib as mpl
 
 run_time = dt.datetime.now().strftime('%Y-%m-%d_%H.%M.%S')
 
+model_file_basic_ae    = 'autoencoder.basic.model'
+model_file_adver_ae    = 'autoencoder.adver.model'
+
 data_file_source       = 'kddcup.data.gz'
 data_file_train        = 'train.data.gz'
 data_file_test_normal  = 'test_normal.data.gz'
 data_file_test_anomaly = 'test_anomaly.data.gz'
-data_file_names = [
+data_file_names        = 'kddcup.data.names.csv'
+data_file_names_source = [
   "duration",
   "protocol_type",
   "service",
@@ -84,25 +92,87 @@ pf = ppr.pformat
 # Model
 ################################################################
 
-class NeuralNet:
-  def __init__(self, args=None):
-    super(NeuralNet, self).__init__()
+class Encoder(nn.Module):
+  def __init__(self, input_size, latent_dim):
+    super(Encoder, self).__init__()
+    self.model = nn.Sequential(
+      nn.Linear(input_size, 128),
+      nn.LeakyReLU(0.2),
+      nn.Linear(128, 64),
+      nn.LeakyReLU(0.2),
+      nn.Linear(64, latent_dim)
+    )
+    
+  def forward(self, x):
+    return self.model(x)
 
-class Autoencoder(tor.nn.Module):
-  def __init__(self, args=None):
+
+class Decoder(nn.Module):
+  def __init__(self, latent_dim, output_size):
+    super(Decoder, self).__init__()
+    self.model = nn.Sequential(
+      nn.Linear(latent_dim, 64),
+      nn.LeakyReLU(0.2),
+      nn.Linear(64, 128),
+      nn.LeakyReLU(0.2),
+      nn.Linear(128, output_size)
+    )
+    
+  def forward(self, x):
+    return self.model(x)
+
+
+class Discriminator(nn.Module):
+  def __init__(self, latent_dim):
+    super(Discriminator, self).__init__()
+    self.model = nn.Sequential(
+      nn.Linear(latent_dim, 64),
+      nn.LeakyReLU(0.2),
+      nn.Linear(64, 32),
+      nn.LeakyReLU(0.2),
+      nn.Linear(32, latent_dim),
+    )
+    
+  def forward(self, x):
+    return self.model(x)
+
+
+class Autoencoder(nn.Module):
+  def __init__(self, input_size, latent_size, output_size):
     super(Autoencoder, self).__init__()
-    self.args = args
+    
+    self.encoder = Encoder(input_size, latent_size)
+    self.decoder = Decoder(latent_size, output_size)
+
+  def forward(self, x):
+    x = self.encoder(x)
+    x = self.decoder(x)
+    return x
+
+
+class AdvAutoencoder(nn.Module):
+  def __init__(self, input_size, latent_size, output_size):
+    super(AdvAutoencoder, self).__init__()
+    self.encoder = Encoder(input_size, latent_size)
+    self.decoder = Decoder(latent_size, output_size)
+    self.discriminator = Discriminator(latent_size)
+
+  def forward(self, x):
+    z = self.encoder(x)
+    reconstructed = self.decoder(z)
+    validity = self.discriminator(z)
+    return reconstructed, validity
 
 
 ################################################################
 # Data
 ################################################################
 
-def load_data(file_name=data_file_source):
+def load_data(file_name=data_file_source, header=data_file_names_source):
   print(f"Loading data {file_name}...")
-  data =  pd.read_csv(data_file_source, 
+  data =  pd.read_csv(file_name, 
                       compression='gzip', 
-                      names=data_file_names, 
+                      names=header, 
                       index_col=False)
   print("Data loaded.")
   print()
@@ -110,11 +180,20 @@ def load_data(file_name=data_file_source):
   return data
 
 
-def save_data(data, file_name):
+def save_data(data, file_name, header):
   print(f"Saving data {file_name}...")
   data.to_csv(file_name,
               compression='gzip',
-              header=data_file_names)
+              header=header,
+              index=False)
+  print("Data saved.")
+  print()
+
+
+def save_header(data, file_name):
+  print(f"Saving column names {file_name}...")
+  data.to_csv(file_name,
+              index=False)
   print("Data saved.")
   print()
 
@@ -143,38 +222,173 @@ def plot_data(data, mode='categorical', feature='label'):
   mpl.pyplot.show()    
 
 
-def clean_data(data):
+def make_tensor(data):
+  return tor.tensor(data.values, dtype=tor.float32)
+
+
+def process_data(data):
   print("Checking data for missing values in columns...")
   if data.isna().any().any():
     print("Missing values found.")
   else:
     print("No missing values.")
+
   print()
   print("Checking data for duplicate rows...")
   print(f"Found {data.duplicated().sum()} duplicates.")
+
   print()
   print("Dropping duplicates...")
-  data_clean = data.drop_duplicates()
+  data = data.drop_duplicates()
+
   print()
-  print_data(data_clean)
-  
-  return data_clean
+  print("One hot encoding categorical features...")
+  data = data.join(pd.get_dummies(data['protocol_type'],
+                                  prefix='protocol_type',
+                                  dtype=int)).drop('protocol_type', axis=1)
+  data = data.join(pd.get_dummies(data['service'],
+                                  prefix='service',
+                                  dtype=int)).drop('service', axis=1)
+  data = data.join(pd.get_dummies(data['flag'],
+                                  prefix='flag',
+                                  dtype=int)).drop('flag', axis=1)
 
+  print()
+  print("Min-max scaling data...")
+  data_num = data.select_dtypes(include=['number'])
+  data_obj = data.select_dtypes(exclude=['number'])
+  data_minmax = data_num.copy()
+  for col in data_num.columns:
+    min_col = data_num[col].min()
+    max_col = data_num[col].max()
+    if min_col != max_col:
+      data_minmax[col] = (data_num[col] - min_col) / (max_col - min_col)
+    else:
+      data_minmax[col] = 0
+  data = pd.concat([data_minmax, data_obj], axis=1)  
 
-def split_data(data):
-  return
-  save_data(data_clean, data_file_train)  
-    
+  print()
+  print("Normalizing 'label' column...")
+  data_normal = data.copy(deep=True)
+  data_normal.label = data_normal.label.replace('normal.', 'normal')
+  data_anomaly = data_normal.copy(deep=True)
+  data_anomaly.label = data_anomaly['label'].apply(lambda x: 'anomaly' if x != 'normal' else x)
+  data_normal = data_normal.loc[data_normal['label'] == 'normal']
+  data_anomaly = data_anomaly.loc[data_anomaly['label'] != 'normal']
+
+  print()
+  print("Splitting data into normal train/test and anomaly test...")
+  data_normal = data_normal.drop('label', axis=1)
+  data_anomaly = data_anomaly.drop('label', axis=1)
+  data_normal_train, data_normal_test = sk.model_selection.train_test_split(data_normal,
+                                                                            test_size=0.2,
+                                                                            random_state=7)
+
+  print()
+  print("Saving normal training and test data and anomaly test data...")
+  save_data(data_normal_train, data_file_train, header=None)
+  save_data(data_normal_test, data_file_test_normal, header=None)
+  save_data(data_anomaly, data_file_test_anomaly, header=None)
+  save_header(data_normal.head(0), data_file_names)
+
+  print("Data processed and saved for training and testing.")
+  print()
+
+  return data_normal_train, data_normal_test, data_anomaly
+
 
 ################################################################
 # Training
 ################################################################
+
+def train(data, model_type='basic', epochs=1, batch_size=64, latent_size=16, learning_rate=0.001):
+  data_tensor = make_tensor(data)
+  dataset = TensorDataset(data_tensor, data_tensor)
+  data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+  # Initialize the model
+  if model_type == 'basic':
+    model = Autoencoder(input_size=data_tensor.shape[1],
+                        latent_size=latent_size,
+                        output_size=data_tensor.shape[1])
+    print("Training basic autoencoder...")
+  elif model_type == 'adver':
+    model = AdvAutoencoder(input_size=data_tensor.shape[1],
+                           latent_size=latent_size,
+                           output_size=data_tensor.shape[1])
+    print("Training adversarial autoencoder...")
+  else:
+    raise ValueError("Invalid model type specified")
+
+  # Criterion and optimizer
+  criterion_reconstruction = nn.MSELoss()
+  criterion_discriminator = nn.BCEWithLogitsLoss()
+  optimizer = opt.Adam(model.parameters(), lr=learning_rate)
+
+  # Training loop
+  for epoch in range(epochs):
+    for inputs, _ in data_loader:
+      if model_type == 'basic':
+        # Forward pass
+        outputs = model(inputs)
+        loss = criterion_reconstruction(outputs, inputs)  
+
+        # Backward pass and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        print(f"Epoch [{epoch+1}/{epochs}], Loss: {loss.item():.4f}")
+
+      elif model_type == 'adver':
+        # Forward pass
+        reconstructed, validity = model(inputs)
+ 
+        # Reconstruction loss
+        reconstruction_loss = criterion_reconstruction(reconstructed, inputs)
+
+        # Discriminator loss for real and fake samples
+        real_samples = tor.randn(batch_size, latent_size)  # Gaussian distribution
+        real_preds = model.discriminator(real_samples)
+        real_loss = criterion_discriminator(real_preds, tor.ones_like(real_preds))
+
+        fake_preds = model.discriminator(validity.detach())
+        fake_loss = criterion_discriminator(fake_preds, tor.zeros_like(fake_preds))
+
+        # Total discriminator loss
+        discriminator_loss = (real_loss + fake_loss) / 2
+
+        # Backpropagation for autoencoder
+        optimizer.zero_grad()
+        reconstruction_loss.backward(retain_graph=True)
+        optimizer.step()
+
+        # Backpropagation for discriminator
+        optimizer.zero_grad()
+        discriminator_loss.backward()
+        optimizer.step()
+
+        print(f"Epoch [{epoch+1}/{epochs}]")
+        print(f"  Reconstruction Loss: {reconstruction_loss.item()}")
+        print(f"  Discriminator Loss: {discriminator_loss.item()}")
+
+  return model
 
 
 ################################################################
 # Testing
 ################################################################
 
+def test():
+  pass
+
+
+################################################################
+# Results
+################################################################
+
+def results():
+  pass
 
 
 ################################################################
@@ -187,28 +401,61 @@ def main():
   print("Autoencoder Research Project")
   print("----------------------------------------------------------------")
   print()
-  print(f"Starting run at {run_time}")
+  print(f"Starting run at {run_time}...")
   print()
 
-  data = load_data()
-  data = clean_data(data)
-  split_data(data)
-  
+  # Check if M1 GPU (or MPS) is available
+  device = tor.device("mps" if tor.backends.mps.is_available() else "cpu")
+  print("Running on device...")
+  print(device)
+  print()
 
+  if (os.path.isfile(data_file_train) and
+      os.path.isfile(data_file_test_normal) and
+      os.path.isfile(data_file_test_anomaly) and
+      os.path.isfile(data_file_names)):
+    print("Data has been processed already, using existing files.")
+    print()
+    data_header = pd.read_csv(data_file_names, nrows=0).columns.tolist()
+    data_normal_train = load_data(data_file_train, header=data_header)
+    data_normal_test  = load_data(data_file_test_normal, header=data_header)
+    data_anomaly      = load_data(data_file_test_anomaly, header=data_header)
+  else:
+    data = load_data()
+    print("Processing data...")
+    print()
+    data_normal_train, data_normal_test, data_anomaly = process_data(data)
+
+  print("Train data")
+  print_data(data_normal_train, True)
+  print(data_normal_train.info())
+  print()
+  print("Test data (normal)")
+  print_data(data_normal_test, True)
+  print(data_normal_test.info())
+  print()
+  print("Test data (anomaly)")
+  print_data(data_anomaly, True) 
+  print(data_anomaly.info())
+  print()
+
+  model_basic = train(data_normal_train, model_type='basic')
+  print()
+  print("Saving basic autoencoder model...")
+  tor.save(model_basic, model_file_basic_ae)
+
+  model_adver = train(data_normal_train, model_type='adver')
+  print()
+  print("Saving adversarial autoencoder model...")
+  tor.save(model_adver, model_file_adver_ae)
+
+  test()
+  results()
+
+  print()
   print("Run complete.")
   print()
   sys.exit()
-
-  # print_data(data.loc[data['label'] == 'normal.'])
-  # print_data(data)
-  # plot_data(data)
-
-   #data = preprocess_data(data)
-
-  # df, labels, num_normal,input_size = preprocess_data(df)
-  # anomal_ratio = 0.001
-  # reduce_data= reduce_data(df, labels, num_normal, anomal_ratio)
-  # print('reduced_data shape = ', reduced_data.shape)
 
   print()
   print("----------------------------------------------------------------")
